351a40715b0118434ba15947651a8fa3f0365b40
Cobra30621 <cobra20621@gmail.com>
2022-05-02 22:08:14 +0800

    update vtuber_banners in media move once method from song_manager to song_manager_once_method

--- datas/song_manager.py
+++ datas/song_manager.py
@@ -10,80 +10,8 @@ from django_pandas.io import read_frame
 from datetime import date
 import datas.google_sheet_manager as google_sheet_manager
 
-##### Vtuber #####
-
-def load_vtuber_csv():
-    vtuber_df = pd.read_csv('./datas/csv/vtuber.csv') 
-    print(vtuber_df)
-
-    start = 0
-    end = len(vtuber_df)
-
-    for i in range(start, end):
-        youtube_id = vtuber_df['channel_id'][i]
-        youtube_url = 'https://www.youtube.com/channel/' + youtube_id
-
-        vtuber = Vtuber(name = vtuber_df['Chanel Name'][i], 
-            youtube_id = youtube_id, 
-            youtube_url=youtube_url, 
-            thumbnail_url=vtuber_df['img_url'][i])
-        vtuber.save()
-
-def load_group_sheet():
-    group_df, group_df_worksheet = google_sheet_manager.get_sheet(google_sheet_manager.group_sheet_id )
-
-    start = 0
-    # end = 2
-    end = len(group_df)
-
-    for i in range(start, end):
-
-        vtuber_str = group_df['Chanel Name'][i]
-        group_str = group_df['Group'][i]
-        vtuber = Vtuber.objects.filter(name = vtuber_str)[0]
-        print(group_str)
-        unit = Group.Unit(group_str)
-
-        group = Group(vtuber = vtuber, unit = unit)
-        group.save()
-        
-
 
 ##### Songs #####
-
-def load_songs_csv():
-    songs_df = pd.read_csv('./datas/csv/songs.csv') 
-    print(songs_df)
-
-    i = 0
-    song_temp = ""
-
-    start = 0
-    # end = 2
-    end = len(songs_df)
-
-    for i in range(start, end):
-
-        song_name = songs_df['title'][i]
-        singer_name = songs_df['singer'][i]
-        singer = Vtuber.objects.filter(name = singer_name)[0]
-
-        if(song_name != song_temp):
-            skip = False
-            if(songs_df['Skip'][i] == 'TRUE'):
-                skip = True
-
-            song = Song(name = song_name, 
-                youtube_id= songs_df['videoId'][i], 
-                thumbnail_url= songs_df['thumbnail_url'][i],
-                youtube_url = songs_df['youtube_url'][i],
-                publish_at = songs_df['publishedAt'][i],
-                skip = skip)
-            song.save()
-            # song.singer.add(singer)
-            song_temp = song_name
-        # else:
-        song.singer.add(singer)
 
 def add_this_week_new_song_to_models():
     # 記得要上傳new_songs.csv
@@ -145,28 +73,6 @@ def add_this_week_new_song_to_models():
 
 ##### Record ######
 
-def load_record_csv():
-    record_df = pd.read_csv('./datas/csv/record.csv') 
-    print(record_df)
-
-    start = 0
-    end = len(record_df)
-
-    for i in range(start, end):
-        youtube_id = record_df['videoId'][i]
-        song = Song.objects.filter(youtube_id = youtube_id)[0]
-
-        view = record_df['view'][i]
-        date = record_df['date'][i]
-
-        record = Record(song = song, 
-            total_view = view,
-            weekly_view = 0, 
-            date=date)
-        record.save()
-
-    add_all_weekly_view_to_record()
-
 def add_this_week_record_to_models(this_date):
 
     
@@ -220,12 +126,6 @@ def add_weekly_view_to_record_by_this_date(this_date):
     for record in records:
         add_weekly_view_to_record(record)
 
-def add_all_weekly_view_to_record():
-    records = Record.objects.all()
-
-    for record in records:
-        add_weekly_view_to_record(record)
-
 def add_weekly_view_to_record(now_record):
     has_find , previous_record = Record.get_previous_record(now_record)
 
@@ -242,6 +142,7 @@ def add_weekly_view_to_record(now_record):
     now_record.weekly_view = weekly_view
     now_record.save()
 
+##### Vtuber Record #########
 
 def add_vtuber_record_by_date(now_date):
     vtubers = Vtuber.objects.all()
@@ -272,101 +173,18 @@ def add_vtuber_record_by_date(now_date):
             
             print(vtuber_record)
 
-def add_init_vtuber_record():
-    vtubers = Vtuber.objects.all()
-    date_list = Record.get_date_list()
 
-    for vtuber in vtubers:
-        for now_date in date_list['date']:
-            print(vtuber, now_date)
-            songs = Song.objects.filter(singer = vtuber).filter(song_records__date = now_date).values('name', 'song_records__total_view', 'song_records__weekly_view')
-            df = read_frame(songs)
-            if(len(df) != 0):
-                total_view_df = df[df['song_records__total_view'] != 0] # 篩掉觀看數為0
-                total_view = total_view_df['song_records__total_view'].sum()
-                song_count = len(total_view_df)
-                average_view  = int(total_view/ song_count)
-
-                weekly_view_df = df[df['song_records__weekly_view'] != 0] # 篩掉觀看數為0
-                total_view_weekly_growth = weekly_view_df['song_records__weekly_view'].sum()
-                weekly_song_count = len(weekly_view_df)
-                average_view_weekly_growth = int(total_view_weekly_growth /weekly_song_count)
-
-                vtuber_record = VtuberRecord(
-                    vtuber = vtuber,
-                    total_view = total_view,
-                    total_view_weekly_growth = total_view_weekly_growth,
-                    average_view = average_view,
-                    average_view_weekly_growth = average_view_weekly_growth,
-                    song_count = song_count,
-                    date = now_date)
-                vtuber_record.save()
-                
-                print(vtuber_record)
-
-import urllib.request
-
-def download_vtuber_img_to_media():
-    youtube = y_api.set_api_key(2) # 使用分帳
-
-    vtuber_df = pd.read_csv('./datas/csv/vtuber.csv') 
-    thumbnails_path = 'media/vtuber_thumbnails/'
-    banners_path = 'media/vtuber_banners/'
-
-    start = 0
-    # end = 2
-    end = len(vtuber_df)
-
-    for i in range(start, end):
-        channel_id = vtuber_df['channel_id'][i]
-        request = youtube.channels().list(
-            part= "snippet,statistics,brandingSettings",   
-            id= channel_id
-        )
-        response = request.execute()
-
-        # 頻道縮圖
-        thumbnails_url = response['items'][0]['snippet']['thumbnails']['medium']['url']
-        thumbnails_file_name = thumbnails_path + channel_id + '.png'
-        urllib.request.urlretrieve(thumbnails_url, thumbnails_file_name)
-
-        # 頻道封面
-        # banners_url = response['items'][0]['brandingSettings']['image']['bannerExternalUrl']
-        # banners_file_name = banners_path + vtuber_df['Chanel Name'][i] + '.png'
-        # urllib.request.urlretrieve(banners_url, banners_file_name)
-        
 
 def test_code():
     a = 1
-    # download_vtuber_img_to_media()
+   
     # 每週要做的事情
     this_date = '2022-5-1'
-    # add_this_week_new_song_to_models()
-    # add_this_week_record_to_models(this_date)
-    # add_weekly_view_to_record_by_this_date(this_date)
+    # add_this_week_new_song_to_models() # 新增新歌到songs
+    # add_this_week_record_to_models(this_date) # 新增本周所有歌曲的record
+    # add_weekly_view_to_record_by_this_date(this_date) # 計算本周歌曲的 weekly record
     
-
-    # print(Record.get_date_list())
-    
-    # records = Record.objects.filter(date = '2022-03-09').values('song__name', 'song__youtube_id', 'total_view')
-    # df = read_frame(records)
-    # print(df)
-
-    # songs = Song.objects.filter(singer__name = 'Suisei Channel').filter(song_records__date =  '2022-03-09').values('name', 'song_records__total_view')
-    # df = read_frame(songs)
-    # print(df)
-    # 'Gen-0'
-
-    # unit = Group.Unit('Gen-0')
-    # print(unit)
-    # vtuber = ''
-
-    # group = Group(vtuber = vtuber, 
-    #     unit = unit)
-    # group.save()
-
-    # add_this_week_new_song_to_models()
-    # new_songs_df, new_song_worksheet = google_sheet_manager.get_sheet(google_sheet_manager.song_sheet_id)
+    add_vtuber_record_by_date(this_date)
 
     
 
