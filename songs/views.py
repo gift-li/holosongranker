60d713803d5a11110f69f9159845eb2fe96345db
gift-li <eug11-family@hotmail.com>
2022-05-16 18:10:59 +0800

    upgrade ranker(singer/song) method; fixed wrong search query after pagination bug

--- songs/views.py
+++ songs/views.py
@@ -12,46 +12,22 @@ from pprint import pp
 
 @require_http_methods(['GET', 'POST'])
 def Ranker(request):
+    # * var: get_records_page()
     default_page = 1
-    order_query = '-weekly_view'
-    date_query = Record.get_lastest_record_date()
+    on_each_side = 2
+    on_ends = 3
+    # * Others
     date_select_list = Record.get_date_list()['date'].tolist()
 
-    if request.method == 'GET':
-        request.session['singers_view_select'] = order_query
-        request.session['singers_date_select'] = datetime.strftime(
-            date_query, "%Y/%m/%d")
+    # 取得搜尋條件
+    order_query, date_query = get_records_search_query(request)
 
-    if request.method == 'POST':
-        request.session['songs_view_select'] = request.POST.get(
-            'view_select')
-        request.session['songs_date_select'] = request.POST.get(
-            'date_select')
+    # 取得資料
+    records = get_records_list(date_query, order_query, order_by='DSC')
 
-        order_query = str('-' + request.POST.get('view_select'))
-        date_query = datetime.strptime(
-            request.POST.get('date_select'), "%Y/%m/%d").date()
-
-    # * 嘗試抓取資料，失敗使用預設抓取
-    try:
-        records = Record.objects.filter(date=date_query) \
-            .prefetch_related('song', 'song__singer', 'song__singer__vtuber_groups') \
-            .order_by(order_query)
-    except:
-        records = Record.objects.filter(date=Record.get_lastest_record_date()) \
-            .prefetch_related('song', 'song__singer', 'song__singer__vtuber_groups') \
-            .order_by('-weekly_view')
-
-    page = request.GET.get('page', default_page)
-    p = Paginator(records, 10)
-    try:
-        records = p.page(page)
-    except PageNotAnInteger:
-        records = p.page(default_page)
-    except EmptyPage:
-        records = p.page(p.num_pages)
-    page_range = p.get_elided_page_range(
-        page, on_each_side=2, on_ends=3)
+    # 分頁
+    records, page_range = get_records_page(
+        request, records, default_page, on_each_side, on_ends)
 
     template = 'songs/ranker.html'
     context = {
@@ -61,3 +37,66 @@ def Ranker(request):
     }
 
     return render(request, template, context)
+
+
+def get_records_search_query(request):
+    order_query = 'weekly_view'
+    date_query = Record.get_lastest_record_date()
+    pp(request.session)
+    if request.method == 'GET':
+        # * 沒快取: 設定為"周觀看"+"最新日期"
+        if 'songs_view_select' not in request.session:
+            request.session['songs_view_select'] = order_query
+            request.session['songs_date_select'] = datetime.strftime(
+                date_query, "%Y/%m/%d")
+
+    if request.method == 'POST':
+        # * 取得POST的搜尋條件
+        request.session['songs_view_select'] = request.POST.get(
+            'view_select')
+        request.session['songs_date_select'] = request.POST.get(
+            'date_select')
+
+    # * 用Session賦值
+    order_query = request.session['songs_view_select']
+    date_query = datetime.strptime(
+        request.session['songs_date_select'], "%Y/%m/%d").date()
+
+    return order_query, date_query
+
+
+def get_records_page(request, data, default_page=1, on_each_side=2, on_ends=3):
+    page = request.GET.get('page', default_page)
+    p = Paginator(data, 10)
+    try:
+        data = p.page(page)
+    except PageNotAnInteger:
+        data = p.page(default_page)
+    except EmptyPage:
+        data = p.page(p.num_pages)
+    page_range = p.get_elided_page_range(
+        page, on_each_side=on_each_side, on_ends=on_ends)
+
+    return data, page_range
+
+
+def get_records_list(date_query, order_query, order_by='DSC'):
+    # * 預設日期: 最新更新日期
+    if (date_query == None):
+        date_query = Record.get_lastest_record_date()
+    # * 順序: 正號 / 逆序: 負號
+    if order_by == 'DSC':
+        order_by = '-'
+    else:
+        order_by = ''
+
+    try:
+        records = Record.objects.filter(date=date_query) \
+            .prefetch_related('song', 'song__singer', 'song__singer__vtuber_groups') \
+            .order_by(str(order_by + order_query))
+    except:
+        records = Record.objects.filter(date=Record.get_lastest_record_date()) \
+            .prefetch_related('song', 'song__singer', 'song__singer__vtuber_groups') \
+            .order_by('-weekly_view')
+
+    return records
