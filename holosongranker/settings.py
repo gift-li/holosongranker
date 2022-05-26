a9941e9063e4e2e183eb44a0790c9a4be888fca1
gift-li <eug11-family@hotmail.com>
2022-05-26 10:57:45 +0800

    fixed deploy bug(v2): staticfiles

--- holosongranker/settings.py
+++ holosongranker/settings.py
@@ -155,6 +155,9 @@ STATICFILES_DIRS = (
     BASE_DIR / "static",
 )
 
+# Static asset configuration.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
