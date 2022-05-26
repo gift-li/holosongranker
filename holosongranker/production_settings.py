6a0782416f61f9dfa0b5ed2a588257d97316c4bc
gift-li <eug11-family@hotmail.com>
2022-05-26 13:41:10 +0800

    DEBUG test(v3): set True; 500 for unknown reason

--- holosongranker/production_settings.py
+++ holosongranker/production_settings.py
@@ -23,11 +23,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers.
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['holosongranker.herokuapp.com', '127.0.0.1']
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Turn off DEBUG mode.
-DEBUG = False
+DEBUG = True
