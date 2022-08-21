5c6520d45f90f1ff709fa3c9e4ce5c364c37cf4c
Cobra30621 <cobra20621@gmail.com>
2022-08-21 22:18:58 +0800

    feat: update 08/21 data

--- datas/manager/weekly_manager.py
+++ datas/manager/weekly_manager.py
@@ -12,7 +12,7 @@ def weekly_work():
     # 先將setting 設為的 DEBUG = True(32)
 
     # 1.去colab 找新歌曲 https://colab.research.google.com/drive/1Ddb4O_2UH5t5ZPkUI9ISygSR3sYGQ3Jv?usp=sharing
-    this_date = '2022-08-14'
+    this_date = '2022-08-21'
 
     
 
@@ -28,29 +28,29 @@ def weekly_work():
     # sc.insert_vtuber_record(this_date)
 
 
-    # # 2.5. 更新VT頭像、頻道縮圖
-    # imgdownloader = ImageDownloader()
-    # # 抓取Vtuber頻道縮圖、封面
-    # imgdownloader.download_vtuber_image()
-    # # 抓取歌曲縮圖
-    # imgdownloader.download_songs_image()
+    # 2.5. 更新VT頭像、頻道縮圖
+    imgdownloader = ImageDownloader()
+    # 抓取Vtuber頻道縮圖、封面
+    imgdownloader.download_vtuber_image()
+    # 抓取歌曲縮圖
+    imgdownloader.download_songs_image()
 
-    # # 3.取的比賽圖所需歌曲資料
-    # # 連結: https://hackmd.io/@Cobra3279/S1qr2Rnb5/%2F5kM-oz_STe22hhlu2YUzvQ
-    # gc = GraphDataCreater()
-    # dates = Record.get_date_list()['date'][:5].tolist()[::-1]
-    # print(dates)
+    # 3.取的比賽圖所需歌曲資料
+    # 連結: https://hackmd.io/@Cobra3279/S1qr2Rnb5/%2F5kM-oz_STe22hhlu2YUzvQ
+    gc = GraphDataCreater()
+    dates = Record.get_date_list()['date'][:5].tolist()[::-1]
+    print(dates)
 
-    # # 歌曲比賽圖
-    # gc.get_songs_for_bar_chart(dates)
-    # gc.get_songs_for_line_chart(dates)
+    # 歌曲比賽圖
+    gc.get_songs_for_bar_chart(dates)
+    gc.get_songs_for_line_chart(dates)
 
-    # # 歌手比賽圖
-    # gc.get_vtubers_for_bar_chart(dates)
-    # gc.get_vtubers_for_line_chart(dates)
+    # 歌手比賽圖
+    gc.get_vtubers_for_bar_chart(dates)
+    gc.get_vtubers_for_line_chart(dates)
 
-    # # # 4. 備份資料
-    # backup_manager.backup_all()
+    # # 4. 備份資料
+    backup_manager.backup_all()
     
     
     # 5.輸出sql檔案
